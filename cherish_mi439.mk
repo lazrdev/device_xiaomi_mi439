55d8022639e3631816b182607b2307d8557317a8
Jake <yapjakebrian1@gmail.com>
2022-03-17 11:16:16 +0800

    Update fingerprint override logic
    Signed-off-by: Jake <yapjakebrian1@gmail.com>
    Signed-off-by: lazr <93397127+lazrdev@users.noreply.github.com>

--- cherish_mi439.mk
+++ cherish_mi439.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The Potato Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,11 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
-
-# Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 720
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
 
 # Inherit from mi439 device
 $(call inherit-product, device/xiaomi/mi439/device.mk)
@@ -23,19 +20,28 @@ PRODUCT_PACKAGES += \
     xiaomi_pine_overlay_lineage \
     xiaomi_olive_overlay_lineage
 
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mi439
-PRODUCT_NAME := cherish_mi439
+PRODUCT_NAME := syberia_mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+DEVICE_MAINTAINER := iaiaian1
+
+# Fingerprint
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys
+BUILD_DESCRIPTION := raven-user 12 SQ1D.211205.016.A1 7957957 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
